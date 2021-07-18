6b8d52f88c284da4c0ebe1a801914baa7e4e8041
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-18 20:07:23 +0300

    imported directive highliter

--- src/app/Directives/highlighter.directive.ts
+++ src/app/Directives/highlighter.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
 })
 export class HighlighterDirective {
 
-  constructor() { }
+  constructor(elem: ElementRef) { 
+    elem.nativeElement.className = 'alert alert-success';
+  }
 
 }
