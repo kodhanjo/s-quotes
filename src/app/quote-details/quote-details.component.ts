827a3cd7e276f8e6bb445e1481147fac68814c78
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-18 20:26:43 +0300

    summarised quote components

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../Boilerplate/Quote'
 
 @Component({
   selector: 'app-quote-details',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class QuoteDetailsComponent implements OnInit {
 
   constructor() { }
-
+  @Input() quote: Quotes;
   ngOnInit(): void {
   }
 
