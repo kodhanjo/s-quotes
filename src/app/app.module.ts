44f7c72f82209dc8b7ccbe2f77663caf218259fb
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-18 19:35:57 +0300

    added quote details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainQuoteComponent } from './main-quote/main-quote.component';
 import { AddQuoteComponent } from './add-quote/add-quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainQuoteComponent,
-    AddQuoteComponent
+    AddQuoteComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
