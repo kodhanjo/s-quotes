eff88c24ffbc2b87eb517747d7d01f258ecf8fea
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-18 19:55:45 +0300

    copleted moments pipe

--- src/app/DateDifference/datedifferernce.pipe.ts
+++ src/app/DateDifference/datedifferernce.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
   name: 'datedifferernce'
 })
 export class DatediffererncePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: moment.Moment, ...args: unknown[]): unknown {
+    const now = moment();
+    return now.diff(value, 'days');
   }
 
 }
