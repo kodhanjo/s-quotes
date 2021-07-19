b3ca831741badf2659261af68e03cfe5d3cf2a7b
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-19 08:19:23 +0300

    building blocks

--- src/app/quotes-add/quotes-add.component.ts
+++ src/app/quotes-add/quotes-add.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Quotes } from '../Boilerplates/Quote';
+import * as moment from 'moment';
 @Component({
   selector: 'app-quotes-add',
   templateUrl: './quotes-add.component.html',
@@ -7,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesAddComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  }
+  
+  @Output() emitQuote = new EventEmitter();
+  quote: string;
+  username: string;
+  author: string;
+  newQuote: any;
 
+  submitQuote(): void {
+    this.newQuote = new Quotes(
+      this.username,
+      this.quote,
+      this.author,
+      moment()
+    );
+    this.quote = '';
+    this.author = '';
+    this.username = '';
+    this.emitQuote.emit(this.newQuote);
+  }
   ngOnInit(): void {
   }
 
